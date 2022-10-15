92832e2ef6f00d7d7e9c9676116efb22b1de6bc1
Sean <seanlengws@gmail.com>
2022-10-15 22:22:23 +0800

    Added prettierrc.json files and removed sw.js files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,78 +1,61 @@
-import Head from 'next/head'
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
+import Head from "next/head"
+import "../styles/globals.css"
 
 //Web3 auth
-import { Chain, createClient, configureChains, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public';
-import { SessionProvider } from 'next-auth/react';
+import { Chain, createClient, configureChains, WagmiConfig } from "wagmi"
+import { alchemyProvider } from "wagmi/providers/alchemy"
+import { publicProvider } from "wagmi/providers/public"
+import { SessionProvider } from "next-auth/react"
 
 const mumbai: Chain = {
   id: 80_001,
-  name: 'Mumbai',
-  network: 'mumbai',
+  name: "Mumbai",
+  network: "mumbai",
   nativeCurrency: {
     decimals: 18,
-    name: 'MATIC',
-    symbol: 'MATIC',
+    name: "MATIC",
+    symbol: "MATIC",
   },
   rpcUrls: {
-    default: 'https://rpc-mumbai.maticvigil.com',
+    default: "https://rpc-mumbai.maticvigil.com",
   },
   testnet: true,
 }
 
-const { provider, webSocketProvider } = configureChains([mumbai],[
-  alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY }),
-  publicProvider(), ]);
-  
-  const client = createClient({
-    provider,
-    webSocketProvider,
-    autoConnect: true,
-  });
+const { provider, webSocketProvider } = configureChains([mumbai], [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY }), publicProvider()])
 
-  type AppProps ={ 
-    Component: any;
-    pageProps: any;
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+})
+
+type AppProps = {
+  Component: any
+  pageProps: any
 }
-
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <WagmiConfig client={client}>
-           <SessionProvider session={pageProps.session} refetchInterval={10000}>
-            <Head>
-                <meta charSet="utf-8" />
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <meta
-                  name="viewport"
-                  content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-                />
-                <meta name="description" content="Description" />
-                <meta name="keywords" content="Keywords" />
-                <title>Eyeseek fund</title>
+        <SessionProvider session={pageProps.session} refetchInterval={10000}>
+          <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+            <meta name="description" content="Description" />
+            <meta name="keywords" content="Keywords" />
+            <title>Eyeseek fund</title>
 
-                <link rel="manifest" href="/manifest.json" />
-                <link
-                  href="/icons/favicon-16x16.png"
-                  rel="icon"
-                  type="image/png"
-                  sizes="16x16"
-                />
-                <link
-                  href="/icons/favicon-32x32.png"
-                  rel="icon"
-                  type="image/png"
-                  sizes="32x32"
-                />
-                <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-                <meta name="theme-color" content="#317EFB" />
-              </Head>
-              <Component {...pageProps} />
-          </SessionProvider>
+            <link rel="manifest" href="/manifest.json" />
+            <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+            <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+            <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+            <meta name="theme-color" content="#317EFB" />
+          </Head>
+          <Component {...pageProps} />
+        </SessionProvider>
       </WagmiConfig>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,101 +1,89 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import styled from "styled-components"
+import { useRouter } from "next/router"
+import Eye7 from "../public/Eye7.png"
+import { useEffect } from "react"
+import Script from "next/script"
 
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styled from 'styled-components'
-import { useRouter } from 'next/router'
-import Eye7 from '../public/Eye7.png'
-import { useEffect} from 'react'
-import Script from 'next/script'
+import { MetaMaskConnector } from "wagmi/connectors/metaMask"
+import { signIn } from "next-auth/react"
+import { useAccount, useConnect, useSignMessage, useDisconnect } from "wagmi"
+import axios from "axios"
 
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { signIn } from 'next-auth/react';
-import { useAccount, useConnect, useSignMessage, useDisconnect } from 'wagmi';
-import axios from 'axios';
-
-import Header from '../sections/Header'
-import Footer from '../sections/Footer'
-import Title from '../components/typography/Title'
-import Subtitle from '../components/typography/Subtitle'
-import Button from '../components/buttons/Button'
-
-
+import Header from "../sections/Header"
+import Footer from "../sections/Footer"
+import Title from "../components/typography/Title"
+import Subtitle from "../components/typography/Subtitle"
+import Button from "../components/buttons/Button"
 
 const Container = styled.div`
   margin-top: 1%;
-`;
-
-const EyeSevenBox = styled.div`
-    text-align: center;
-    position: relative;
 `
 
-
+const EyeSevenBox = styled.div`
+  text-align: center;
+  position: relative;
+`
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-  }, []);
-
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+  }, [])
 
   // Web3 Auth handling
-  const { connectAsync } = useConnect();
-  const { disconnectAsync } = useDisconnect();
-  const { isConnected } = useAccount();
-  const { signMessageAsync } = useSignMessage();
-  const { push } = useRouter();
-
+  const { connectAsync } = useConnect()
+  const { disconnectAsync } = useDisconnect()
+  const { isConnected } = useAccount()
+  const { signMessageAsync } = useSignMessage()
+  const { push } = useRouter()
 
   const handleAuth = async () => {
     if (isConnected) {
-        await disconnectAsync();
+      await disconnectAsync()
     }
 
-    const { account, chain } = await connectAsync({ connector: new MetaMaskConnector() });
+    const { account, chain } = await connectAsync({ connector: new MetaMaskConnector() })
 
-    const userData = { address: account, chain: chain.id, network: 'evm' };
+    const userData = { address: account, chain: chain.id, network: "evm" }
     console.log(userData)
 
-    const { data } = await axios.post('/api/auth/request-message', userData, {
-        headers: {
-            'content-type': 'application/json',
-        },
-    });
+    const { data } = await axios.post("/api/auth/request-message", userData, {
+      headers: {
+        "content-type": "application/json",
+      },
+    })
 
-    const message = data.message;
+    const message = data.message
 
-    const signature = await signMessageAsync({ message });
+    const signature = await signMessageAsync({ message })
 
     // redirect user after success authentication to '/dashboard' page
     // @ts-ignore
-    const { url } = await signIn('credentials', { message, signature, redirect: false, callbackUrl: '/user' });
+    const { url } = await signIn("credentials", { message, signature, redirect: false, callbackUrl: "/user" })
     /**
      * instead of using signIn(..., redirect: "/dashboard")
      * we get the url from callback and push it to the router to avoid page refreshing
      */
-    push(url);
-};
+    push(url)
+  }
 
   return (
-    
     <Container>
       <Head>
         <title>Eyeseek Fund</title>
         <meta name="title" content="Blockchain crowdfunding application powered by Moralis" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <Title text='Components/Typography/Title'/>
-      <Subtitle text='Components/Typography/Subtitle'/>
-      <Button text={'Components/buttons/button'}/>
-      <EyeSevenBox><Image
-            src={Eye7}
-            alt="Eye7"
-            width={'600%'}
-            height={'70%'}
-            />
-            </EyeSevenBox>
-      <Footer/>
+      <Header />
+      <Title text="Components/Typography/Title" />
+      <Subtitle text="Components/Typography/Subtitle" />
+      <Button text={"Components/buttons/button"} />
+      <EyeSevenBox>
+        <Image src={Eye7} alt="Eye7" width={"600%"} height={"70%"} />
+      </EyeSevenBox>
+      <Footer />
       {/* <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-TKH8YE4L07"
         strategy="afterInteractive"
@@ -108,12 +96,6 @@ const Home: NextPage = () => {
           gtag('config', 'G-TKH8YE4L07');
         `}
       </Script> */}
-
-
-     
-
-
-            
     </Container>
   )
 }
