8465b83a568b3eec582787fbb22c3bc3db6df06c
Sean <seanlengws@gmail.com>
2022-10-19 18:18:58 +0800

    Format all codes

--- components/form/Stepper.js
+++ components/form/Stepper.js
@@ -1,80 +1,98 @@
-import styled from "styled-components";
-import { CheckMark } from "../icons/MobileSet";
+import styled from "styled-components"
+import { CheckMark } from "../icons/MobileSet"
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    margin-top: 2%;
-    margin-bottom: 2%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 2%;
+  margin-bottom: 2%;
 `
 
 const Circle = styled.div`
-    display: flex;
-    justify-content: center;
-    align-self: center;
-    width: 40px;
-    height: 40px;
-    background: #000535;
-    border: 1px solid #FFFFFF;
-    box-sizing: border-box;
-    border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  width: 40px;
+  height: 40px;
+  background: #000535;
+  border: 1px solid #ffffff;
+  box-sizing: border-box;
+  border-radius: 50%;
 `
 
 const NotCircle = styled(Circle)`
-    background: #3A3A3A;
-    &:hover{
-        cursor: pointer;
-        opacity: 0.8;
-    }
+  background: #3a3a3a;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
 `
 
 const YesCircle = styled(Circle)`
-    background: #008D39;
-    &:hover{
-        cursor: pointer;
-        opacity: 0.8;
-    }
+  background: #008d39;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
 `
 
 const Line = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-self: center;
-    height: 1px;
-    width: 50px;
-    margin-left: 10px;
-    margin-right: 10px;
-    background: #C8C8C8;
-    animation: fadeIn 0.5s;
-    @keyframes fadeIn {
-        0% { opacity: 0; }
-        100% { opacity: 1; }
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  height: 1px;
+  width: 50px;
+  margin-left: 10px;
+  margin-right: 10px;
+  background: #c8c8c8;
+  animation: fadeIn 0.5s;
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
     }
+    100% {
+      opacity: 1;
+    }
+  }
 `
 
+const Stepper = ({ increment, decrement, steps, step }) => {
+  const Step = ({ s }) => {
+    return (
+      <>
+        {s < step && (
+          <YesCircle onClick={decrement}>
+            <CheckMark width={15} />
+          </YesCircle>
+        )}
+        {s === step && <Circle />}
+        {s > step && <NotCircle onClick={increment} />}
+      </>
+    )
+  }
 
-const Stepper = ({increment, decrement,steps, step}) => {
-
-    const Step = ({s}) => {
-        return <>
-            {s < step && <YesCircle onClick={decrement}><CheckMark width={15}/></YesCircle>}
-            {s === step && <Circle/>}
-            {s > step && <NotCircle onClick={increment}/>}
+  return (
+    <Container>
+      <Step s={1} />
+      <Line />
+      <Step s={2} />
+      <Line />
+      <Step s={3} />
+      {steps >= 4 && (
+        <>
+          <Line />
+          <Step s={4} />
+          {steps >= 5 && (
+            <>
+              <Line />
+              <Step s={5} />
+            </>
+          )}
         </>
-    }
-
-    return <Container>
-            <Step s={1}/>
-            <Line/><Step s={2}/>
-            <Line/><Step s={3}/>
-        {steps >= 4 && <>
-            <Line/><Step s={4}/>
-            {steps >= 5 && <><Line/><Step s={5}/></>}
-        </>}
-            
-
+      )}
     </Container>
+  )
 }
 
-export default Stepper;
+export default Stepper
--- components/icons/Skeletons.js
+++ components/icons/Skeletons.js
@@ -1,8 +1,9 @@
-export const ImgSkel = ({width, height}) => {
-    return <svg width={width} height={height} viewBox="0 0 603 339" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="1.5" y="1.5" width="599" height="337" rx="4.5" stroke="#3A3A3A"/>
-    <line x1="1.24541" y1="0.564367" x2="601.245" y2="338.564" stroke="#373737"/>
-    <line x1="601.895" y1="2.59734" x2="0.592728" y2="338.276" stroke="#373737"/>
+export const ImgSkel = ({ width, height }) => {
+  return (
+    <svg width={width} height={height} viewBox="0 0 603 339" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="1.5" y="1.5" width="599" height="337" rx="4.5" stroke="#3A3A3A" />
+      <line x1="1.24541" y1="0.564367" x2="601.245" y2="338.564" stroke="#373737" />
+      <line x1="601.895" y1="2.59734" x2="0.592728" y2="338.276" stroke="#373737" />
     </svg>
-    
+  )
 }
