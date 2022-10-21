4cd649cdc749c8172a51b98ff5369d1502859281
dan438 <obafemisope438@gmail.com>
2022-10-21 13:14:47 +0100

    update serverURL and appId

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import Head from "next/head"
-import Header from "../sections/Header"
-import "../styles/globals.css"
+import Head from "next/head";
+import Header from "../sections/Header";
+import "../styles/globals.css";
 
 //Web3 auth
-import { Chain, createClient, configureChains, WagmiConfig } from "wagmi"
-import { AppProvider } from "../sections/utils/appContext"
-import { alchemyProvider } from "wagmi/providers/alchemy"
-import { publicProvider } from "wagmi/providers/public"
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { SessionProvider } from "next-auth/react"
+import { Chain, createClient, configureChains, WagmiConfig } from "wagmi";
+import { AppProvider } from "../sections/utils/appContext";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { SessionProvider } from "next-auth/react";
 import { MoralisProvider } from "react-moralis";
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 
 const mumbai: Chain = {
   id: 80_001,
@@ -25,49 +25,50 @@ const mumbai: Chain = {
     default: "https://rpc-mumbai.maticvigil.com",
   },
   testnet: true,
-}
+};
 
-
-const { provider, webSocketProvider } = configureChains([mumbai], [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY }), publicProvider()])
-
+const { provider, webSocketProvider } = configureChains(
+  [mumbai],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY }), publicProvider()]
+);
 
 const client = createClient({
   provider,
   webSocketProvider,
-  autoConnect: true
-})
+  autoConnect: true,
+});
 
 type AppProps = {
-  Component: any
-  pageProps: any
-}
+  Component: any;
+  pageProps: any;
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <WagmiConfig client={client}>
-        <MoralisProvider appId="4PdSQUwrX1404TxN641gEwmXZqZFpv8CzBIc4FLN" serverUrl="https://aa6nfdqx573p.usemoralis.com:2053/server">
+        <MoralisProvider appId="M7wQiYLcbda4yH7hVX2W5ksbJo4xS74oNVKGZk0C" serverUrl="https://wgdujpk9pprx.grandmoralis.com:2053/server">
           <SessionProvider session={pageProps.session} refetchInterval={10000}>
-              <Head>
-                <meta charSet="utf-8" />
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-                <meta name="description" content="Description" />
-                <meta name="keywords" content="Keywords" />
-                <title>Eyeseek fund</title>
+            <Head>
+              <meta charSet="utf-8" />
+              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+              <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+              <meta name="description" content="Description" />
+              <meta name="keywords" content="Keywords" />
+              <title>Eyeseek fund</title>
 
-                <link rel="manifest" href="/manifest.json" />
-                <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-                <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-                <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-                <meta name="theme-color" content="#317EFB" />
-              </Head>
-              <AppProvider>
-                <Component {...pageProps} />
-              </AppProvider>
+              <link rel="manifest" href="/manifest.json" />
+              <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+              <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+              <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+              <meta name="theme-color" content="#317EFB" />
+            </Head>
+            <AppProvider>
+              <Component {...pageProps} />
+            </AppProvider>
           </SessionProvider>
         </MoralisProvider>
       </WagmiConfig>
     </>
-  )
+  );
 }
