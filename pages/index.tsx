2587e2187a4855c277349fdad9161e3fd63f626b
dan438 <obafemisope438@gmail.com>
2022-10-21 13:07:51 +0100

    complete donate UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,48 @@
-import type { NextPage } from "next"
-import Head from "next/head"
-import Image from "next/image"
-import styled from "styled-components"
-import Eye7 from "../public/Eye7.png"
-import { useEffect, useState } from "react"
-import Script from "next/script"
-import axios from "axios"
-
-import Footer from "../sections/Footer"
-import Features from "../sections/Landing/Features"
-import LatestProjects from "../sections/Landing/LatestProjects"
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styled from "styled-components";
+import Eye7 from "../public/Eye7.png";
+import { useEffect, useState } from "react";
+import Script from "next/script";
+import axios from "axios";
+import Header from "../sections/Header";
+import Footer from "../sections/Footer";
+import Features from "../sections/Landing/Features";
+import LatestProjects from "../sections/Landing/LatestProjects";
 
 const Container = styled.div`
   margin-top: 1%;
-`
+`;
 
 const EyeSevenBox = styled.div`
   text-align: center;
   position: relative;
-`
+`;
 
 const Home: NextPage = () => {
-  const [projects,setProjects] = useState([])
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
-    getProjects()
-  }, [])
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    getProjects();
+  }, []);
 
   // How to query categories https://aa6nfdqx573p.usemoralis.com:2053/server/classes/ProjectTest?where={%22category%22:"some category"}
   // Similar way possible to filter max if needed
   const getProjects = async () => {
-      const config = {
+    const config = {
       headers: {
-        "X-Parse-Application-Id": "4PdSQUwrX1404TxN641gEwmXZqZFpv8CzBIc4FLN"
-      }
+        "X-Parse-Application-Id": "4PdSQUwrX1404TxN641gEwmXZqZFpv8CzBIc4FLN",
+      },
+    };
+    try {
+      const res = await axios.get("https://aa6nfdqx573p.usemoralis.com:2053/server/classes/ProjectTest?", config);
+      setProjects(res.data.results);
+    } catch (error) {
+      console.log(error);
     }
-    try{
-      const res = await axios.get("https://aa6nfdqx573p.usemoralis.com:2053/server/classes/ProjectTest?", config)
-      setProjects(res.data.results)
-    } catch (error){
-      console.log(error)
-    }
-  }
-
+  };
 
   return (
     <Container>
@@ -53,8 +52,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Features/>
-      <LatestProjects data={projects}/>
+      <Features />
+      <LatestProjects data={projects} />
       <EyeSevenBox>
         <Image src={Eye7} alt="Eye7" width={"600%"} height={"70%"} />
       </EyeSevenBox>
@@ -72,7 +71,7 @@ const Home: NextPage = () => {
         `}
       </Script> */}
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
