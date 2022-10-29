a62625695f38e9712cc6b430ea24c54ce303ccbb
maweiche <maweiche@gmail.com>
2022-10-28 20:40:58 -0400

    added styled stats table with dummy data. created all necessary covalent api calls

--- components/statsTable/statsTable.js
+++ components/statsTable/statsTable.js
@@ -2,25 +2,23 @@ import styled from "styled-components"
 import { 
     getLatestBlockHeight,
     getLogEvents, 
-    getERC20Transfers,
-    getTokenTransfers
-    // getNFTExternalMetadata
 } from "../../pages/api/covalent"
 import { useEffect, useState } from "react"
 import Button from "../buttons/Button"
+import Title  from "../typography/Title"
+import SectionTitle from "../typography/SectionTitle"
+import Subtitle from "../typography/Subtitle"
 
 const StatsTable = () => {
-    const [latestBlockHeight, setLatestBlockHeight] = useState(0)
-    const [previousBlockHeight, setPreviousBlockHeight] = useState(0) //based on 24 hour average
-    const [changesInTokenHolders, setChangesInTokenHolders] = useState(0)
-    const [tokenTransfers, setTokenTransfers] = useState(0)
-
     const [showHottestTable, setShowHottestTable] = useState(false);
     const [showTopChainTable, setShowTopChainTable] = useState(false);
 
     const Container = styled.div`
         padding-bottom: 2%;
         padding-top: 2%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     `
 
     const I = styled.input`
@@ -31,28 +29,39 @@ const StatsTable = () => {
     const Table = styled.table`
         width: 100%;
         border-collapse: collapse;
-        border: 1px solid black;
+        border: none;
         text-align: center;
     `
     const Header = styled.th`
-        border: 1px solid black;
+        border: none;
         padding: 1%;
         background-color: transparent;
+        font-family: "Roboto";
     `
     const Row = styled.tr`
-        border: 1px solid black;
         padding: 1%;
+        border-bottom: 1px solid grey;
     `
     const Cell = styled.td`
-        border: 1px solid black;
         padding: 1%;
+        font-family: "Roboto";
+
     `
+
+    const Logo = styled.img`
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+    `
+
 
 
     // ******DUMMY DATA FOR NOW******************************
-    const hottestProjectsData = [
+    const HottestProjectsData = [
         {
-            name: "Project 1",
+            title: "Project 1",
+            category: "Subcategory",
+            logo: "https://cryptologos.cc/logos/thumbs/polygon.png?v=022",
             values: {
                 "24hrTxns": 100,
                 "Microfunds": 100,
@@ -60,7 +69,9 @@ const StatsTable = () => {
             }
         },
         {
-            name: "Project 2",
+            title: "Project 2",
+            category: "Subcategory",
+            logo: "https://cryptologos.cc/logos/thumbs/bnb.png?v=022",
             values: {
                 "24hrTxns": 200,
                 "Microfunds": 200,
@@ -68,7 +79,9 @@ const StatsTable = () => {
             }
         },
         {
-            name: "Project 3",
+            title: "Project 3",
+            category: "Subcategory",
+            logo: "https://cryptologos.cc/logos/thumbs/fantom.png?v=022",
             values: {
                 "24hrTxns": 300,
                 "Microfunds": 300,
@@ -79,7 +92,9 @@ const StatsTable = () => {
     
     const TopChainReactionsData = [
         {
-            transaction: "txn1",
+            title: "txn1",
+            category: "Subcategory",
+            logo: "https://cryptologos.cc/logos/thumbs/polygon.png?v=022",
             values: {
                 "donation": 100,
                 "impact": 100,
@@ -88,105 +103,148 @@ const StatsTable = () => {
             }
         },
         {
-            transaction: "txn2",
+            title: "txn2",
+            category: "Subcategory",
+            logo: "https://cryptologos.cc/logos/thumbs/bnb.png?v=022",
             values: {
                 "donation": 200,
                 "impact": 200,
                 "multiplier": 200,
-                "date": "2020-01-02"
+                "date": "2022-10-25T14:04:57Z"
             }
         },
         {
-            transaction: "txn3",
+            title: "txn3",
+            category: "Subcategory",
+            logo: "https://cryptologos.cc/logos/thumbs/fantom.png?v=022",
             values: {
                 "donation": 300,
                 "impact": 300,
                 "multiplier": 300,
-                "date": "2020-01-03"
+                "date": "2022-10-25T14:04:57Z"
             }
         }
     ]
     //********************************* */
+
+
+//     create function that formats date from 2022-10-25T14:04:57Z to Mar 23, 2022 12:04
+    const formatDate = (date) => {
+        const dateObj = new Date(date);
+        const month = dateObj.toLocaleString('default', { month: 'short' });
+        const day = dateObj.getDate();
+        const year = dateObj.getFullYear();
+        const time = dateObj.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+        return `${month} ${day}, ${year} ${time}`;
+    }
+
+    const getTable = () => {
+        switch (true) {
+            case showHottestTable:
+                return (
+                    <Container>
+                        <Title text="Hottest Projects" />
+                        <Table>
+                            <thead>
+                                <Row>
+                                    <Header></Header>
+                                    <Header>Project Info</Header>
+                                    <Header>Transactions 24h</Header>
+                                    <Header>Microfunds</Header>
+                                    <Header>Deadline</Header>
+                                </Row>
+                            </thead>
+                            <tbody>
+                                {HottestProjectsData.map((project, index) => {
+                                    return (
+                                        <Row key={index}>
+                                            <Logo src={project.logo} alt={'logo'} />
+                                            <Cell>
+                                                {project.title}
+                                                <p style={{ fontStyle: "italic" }}>
+                                                    {project.category}
+                                                </p>
+                                            </Cell>
+                                            <Cell>{project.values["24hrTxns"]}x</Cell>
+                                            <Cell style={{color: '#00EF8B'}}>{project.values["Microfunds"]}x</Cell>
+                                            <Cell>{formatDate(project.values["Deadline"])}</Cell>
+                                        </Row>
+                                    )
+                                })}
+                            </tbody>
+                        </Table>
+                    </Container>
+                )
+            case showTopChainTable:
+                return (
+                    <Container>
+                        <Title text="Top Chain Reactions"/>
+                        <Table>
+                            <thead>
+                                <Row>
+                                    
+                                    <Header></Header>
+                                    <Header>Transaction</Header>
+                                    <Header>Donation</Header>
+                                    <Header>Impact</Header>
+                                    <Header>Multiplier</Header>
+                                    <Header>Date</Header>
+                                </Row>
+                            </thead>
+                            <tbody>
+                                {TopChainReactionsData.map((transaction, index) => {
+                                    return (
+                                        <Row key={index}>
+                                            <Logo src={transaction.logo} alt={'logo'} />
+                                            <Cell>
+                                                {transaction.title}
+                                                <p style={{ fontStyle: "italic" }}>
+                                                    {transaction.category}
+                                                </p>
+                                            </Cell>
+                                            <Cell>${transaction.values["donation"]}</Cell>
+                                            <Cell>${transaction.values["impact"]}</Cell>
+                                            <Cell style={{color: '#00EF8B'}}>{transaction.values["multiplier"]}x</Cell>
+                                            <Cell>{formatDate(transaction.values["date"])}</Cell>
+                                        </Row>
+                                    )
+                                })}
+                            </tbody>
+                        </Table>
+                    </Container>
+                )
+
+            default:
+                return (
+                    <></>
+                )
+        }
+                 
+    }
     
 
     useEffect(() => {
         const getData = async () => {
+
             const latestBlockHeight = await getLatestBlockHeight();
             console.log('latest blockheight',latestBlockHeight)
-            setLatestBlockHeight(latestBlockHeight)
-            setPreviousBlockHeight(latestBlockHeight - 43200) // 24 hours average on polygon
+            
             const logEvents = await getLogEvents(latestBlockHeight - 1, latestBlockHeight);
             console.log('log events', logEvents)
+
         }
         getData();
     }, [])
 
     return (
         <div>
-            <div>
-                <div>Latest Block Height: {latestBlockHeight}</div>
-            </div>
-            <Button text={"Hottest Projects"} onClick={() => setShowHottestTable(!showHottestTable)}/>
-            <Button text={"Top Chain Reactions"} onClick={() => setShowTopChainTable(!showTopChainTable)}/>
-        
-            {showHottestTable && (
-                <Container>
-                    <Table>
-                        <thead>
-                            <Row>
-                                <Header>Project Info</Header>
-                                <Header>Transactions 24h</Header>
-                                <Header>Microfunds</Header>
-                                <Header>Deadline</Header>
-                            </Row>
-                        </thead>
-                        <tbody>
-                            {hottestProjectsData.map((project, index) => {
-                                return (
-                                    <Row key={index}>
-                                        <Cell>{project.name}</Cell>
-                                        <Cell>{project.values["24hrTxns"]}</Cell>
-                                        <Cell>{project.values["Microfunds"]}</Cell>
-                                        <Cell>{project.values["Deadline"]}</Cell>
-                                    </Row>
-                                )
-                            })}
-                        </tbody>
-                    </Table>
-                </Container>
-            )}
-            {showTopChainTable && (
-                <Container>
-                    <Table>
-                        <thead>
-                            <Row>
-                                <Header>Transaction</Header>
-                                <Header>Donation</Header>
-                                <Header>Impact</Header>
-                                <Header>Multiplier</Header>
-                                <Header>Date</Header>
-                            </Row>
-                        </thead>
-                        <tbody>
-                            {TopChainReactionsData.map((transaction, index) => {
-                                return (
-                                    <Row key={index}>
-                                        <Cell>{transaction.transaction}</Cell>
-                                        <Cell>{transaction.values["donation"]}</Cell>
-                                        <Cell>{transaction.values["impact"]}</Cell>
-                                        <Cell>{transaction.values["multiplier"]}</Cell>
-                                        <Cell>{transaction.values["date"]}</Cell>
-                                    </Row>
-                                )
-                            })}
-                        </tbody>
-                    </Table>
-                </Container>
-            )}
+            <Button text={"Hottest Projects"} onClick={() => (setShowHottestTable(!showHottestTable), setShowTopChainTable(false))}/>
+            <Button text={"Top Chain Reactions"} onClick={() => (setShowTopChainTable(!showTopChainTable), setShowHottestTable(false))}/>
+            {getTable()}
         </div>
     )
 }
 
-export default StatsTable
+export default StatsTable;
 
 // Path: components/statsTable/statsTable.js
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ import Footer from "../sections/Footer"
 import Title from "../components/typography/Title"
 import Subtitle from "../components/typography/Subtitle"
 import Button from "../components/buttons/Button"
-import StatsTable from "../components/statsTable/statsTable"
+
 const Container = styled.div`
   margin-top: 1%;
 `
@@ -98,7 +98,7 @@ const Home: NextPage = () => {
       <EyeSevenBox>
         <Image src={Eye7} alt="Eye7" width={"600%"} height={"70%"} />
       </EyeSevenBox>
-      <StatsTable />
+
 
 
       <Footer />
